6f055eb53c8fa9fac2dd0e242bf926dbc6c6dd70
tsuchimore1 <yoshi1003to216@gmail.com>
2016-10-23 09:09:46 +0000

    “コミットの内容”

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,9 +9,9 @@ class ContactsController < ApplicationController
 
  def create
     @contact = Contact.new(contacts_params)
-    @contact.user_id = current_user.id
+
     if @contact.save
-      redirect_to contacts_path, notice: "ブログを作成しました！"
+      redirect_to root_path, notice: "お問い合わせしました！"
       NoticeMailer.sendmail_contact(@contact).deliver
     else
       render action: 'new'
